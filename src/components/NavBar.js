cedc6257d6bdd21bac5c5ea10f663e927c05036e
Danila Legkobytov <bjorn-bis@yandex.ru>
2023-04-10 11:14:23 +0700

    bugfix: Исправить навигацию в NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,46 +1,51 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 // NAVIGATION BAR COMPONENT
 function NavBar({ email, onHamburgerClick, isOpen, onLogOut }) {
-  // VARIABLES WITH HOOKS
-  const location = useLocation();
-  // HANDLE ELEMENT TOGGLE
-  function handleElementToggle() {
-    if (location.pathname === "/sign-in") {
-      return (
-        <Link to="/sign-up" className="header__link">
-          Регистрация
-        </Link>
-      );
-    } else if (location.pathname === "/sign-up") {
-      return (
-        <Link to="/sign-in" className="header__link">
-          Войти
-        </Link>
-      );
-    } else {
-      return (
-        <>
-          <p className="header__user-email">{email || ""}</p>
-          <button
-            type="button"
-            className="header__btn-sign-out"
-            onClick={onLogOut}
-          >
-            Выйти
-          </button>
-          <button
-            type="button"
-            className={`header__btn-hamburger ${
-              isOpen ? "header__btn-hamburger_type_close" : ""
-            }`}
-            onClick={onHamburgerClick}
-          ></button>
-        </>
-      );
-    }
-  }
-  return <div className="header__nav">{handleElementToggle()}</div>;
+  return (
+    <div className="header__nav">
+      <Routes>
+        <Route
+          path="/sign-up"
+          element={
+            <Link to="/sign-in" className="header__link">
+              Войти
+            </Link>
+          }
+        />
+        <Route
+          path="/sign-in"
+          element={
+            <Link to="/sign-up" className="header__link">
+              Регистрация
+            </Link>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <>
+              <p className="header__user-email">{email || ""}</p>
+              <button
+                type="button"
+                className="header__btn-sign-out"
+                onClick={onLogOut}
+              >
+                Выйти
+              </button>
+              <button
+                type="button"
+                className={`header__btn-hamburger ${
+                  isOpen ? "header__btn-hamburger_type_close" : ""
+                }`}
+                onClick={onHamburgerClick}
+              ></button>
+            </>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default NavBar;
